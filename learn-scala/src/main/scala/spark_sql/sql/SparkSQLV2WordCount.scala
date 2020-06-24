0225f7200d71fc2ebb32eb292490205be75bf318
“huangqihang” <huangqihang@wecash.net>
2020-06-25 00:47:44 +0800

    update scala in idea

--- learn-scala/src/main/scala/spark_sql/sql/SparkSQLV2WordCount.scala
+++ learn-scala/src/main/scala/spark_sql/sql/SparkSQLV2WordCount.scala
@@ -22,20 +22,43 @@ object SparkSQLV2WordCount {
     import session.implicits._
     val words = lines.flatMap(_.split("\\s+"))
 
+    //bySQL(session, words)
+
+    byDataSetAPI(session, words)
+
+    session.stop()
+
+  }
+
+  def bySQL(session: SparkSession, words: Dataset[String]): Unit = {
     // 创建视图-虚拟表
     words.createTempView("v_words")
 
     // 执行SQL
     val result = session.sql(
       """
-        select value as word, count(*) as cnt 
-          from v_words 
-          group by value 
+        select value as word, count(*) as cnt
+          from v_words
+          group by value
           order by cnt desc""")
 
+    // 输出结果
     result.show()
-
-    session.stop()
-
   }
+
+  def byDataSetAPI(session: SparkSession, words: Dataset[String]): Unit = {
+    import session.implicits._
+
+    // 使用DataSet的API进行数据计算
+    //val result = words.groupBy($"value" as "word").count().sort($"count" desc)
+
+    import org.apache.spark.sql.functions._
+    val result = words.groupBy($"value" as "word")
+                      .agg(count("*") as "cnt")
+                      .orderBy($"cnt" desc)
+
+    // 输出结果
+    result.show()
+  }
+
 }
